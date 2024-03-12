395bf9f93b1b546adc0f3ff750703f6a939083b3
Blake.Liu <Blake.Liu@hualiteq.com>
2024-03-12 17:43:51 +0800

    新增收支紀錄page

---
+++ app/(routes)/records/create/page.tsx
@@ -0,0 +1,25 @@
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+
+export default function Page() {
+  return (
+    <div className="bg-background">
+      <Tabs defaultValue="expense">
+        <div className="flex items-center justify-between">
+          <div className="flex flex-1">
+            <h1 className="text-xl font-bold">新增收支紀錄</h1>
+          </div>
+          <TabsList className="inline-flex flex-1 ">
+            <TabsTrigger className="w-full" value="income">
+              收入
+            </TabsTrigger>
+            <TabsTrigger className="w-full" value="expense">
+              支出
+            </TabsTrigger>
+          </TabsList>
+        </div>
+        <TabsContent value="income"></TabsContent>
+        <TabsContent value="expense"></TabsContent>
+      </Tabs>
+    </div>
+  );
+}
--- app/(routes)/records/page.tsx
+++ app/(routes)/records/page.tsx
@@ -1,10 +1,19 @@
 import DatePickerBar from "@/app/ui/datePickerBar";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Link from "next/link";
 
 export default async function Page() {
   return (
     <div className="bg-background">
-      <h1 className="text-2xl font-bold">收支紀錄</h1>
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl font-bold">收支紀錄</h1>
+        <Link
+          href="/records/create"
+          className="bg-sky-600	 py-1 text-white font-bold rounded-lg px-4 hover:bg-sky-500 shadow-md"
+        >
+          新增紀錄
+        </Link>
+      </div>
       <Tabs defaultValue="listView" className="w-full mt-2">
         <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="listView">清單</TabsTrigger>
